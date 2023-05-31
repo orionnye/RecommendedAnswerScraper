da110a8e9d68cfc7ba626a302cbef577a245c687
orionnye <orion.nye@gmail.com>
2023-05-31 16:14:48 -0700

    backing up code

--- scraper.js
+++ scraper.js
@@ -10,8 +10,11 @@ let questionList = [
 let badResponseList = [
     "People also ask",
     "More to ask",
-    "Others want to know"
+    "Others want to know",
+    "Related questions",
+    "Images"
 ]
+const answerRegExp = new RegExp(/<span class="FCUp0c rQMQod">(.*?)<\/span>/g)
 // URL of the page we want to scrape
 const urlBase = "https://www.google.com/search?q="
 var getURL = (question, uni) => { return urlBase + question.replace("*", uni) }
@@ -43,18 +46,24 @@ async function retrieveUniData(question, uni) {
         let all = $("*")
         //just use string functions to find first span containing our answer,
         //in browser it's a bold div but cheerio converts it to "span"
-        const answerRegExp = new RegExp(/<span class="FCUp0c rQMQod">(.*?)<\/span>/g)
-        let spanAnswers = all.toString().match(answerRegExp)
-        let spanAnswer = "Nothing Found"
+        let spanAnswers = Array.from(all.toString().matchAll(answerRegExp)).values()
+        let answer
         if (spanAnswers != null) {
-            spanAnswer = spanAnswers.shift()
-            if (spanAnswer.endsWith(".")) {
-                spanAnswer += spanAnswers.shift()
+            for (let match of spanAnswers) {
+                response = match[0].slice(28, match[0].length-7 )
+                if (!badResponseList.includes(response)) {
+                    answer = response
+                    break
+                }
             }
+            // if (spanAnswer.endsWith(".") | spanAnswer.endsWith(" ")) {
+            //     spanAnswer += spanAnswers[1][0]
+            // }
         }
         //Slice off the span since we know the size of span start and end
-        let answer = spanAnswer.slice(28, spanAnswer.length-7 )
+        // let answer = spanAnswer.slice(28, spanAnswer.length-7 )
         
+        // resolved(answer)
         resolved(answer)
     });
 }
@@ -66,79 +75,13 @@ async function scrapeData() {
     let uniAnswers = []
     for (let uni of uniList) {
         await timeout(1000)
-        const answer = await retrieveUniData(questionList[1], uni)
-        console.log(uni,":", answer)
-        uniAnswers.push(answer)
+        const answer = await retrieveUniData(questionList[0], uni)
+        if (answer != null && answer != undefined) {
+            console.log(uni,":", answer)
+            uniAnswers.push(answer)
+        }
     }
     // const answer = await retrieveUniData(questionList[0], uniList[0])
     // console.log(answer)
 }
 scrapeData()
-// console.log(scrapeData())
-// Async function which scrapes the data
-// async function scrapeData() {
-//   try {
-//     // Fetch HTML of the page we want to scrape
-//     const { data } = await axios.get(url);
-//     // Load HTML we fetched in the previous line
-//     const $ = cheerio.load(data);
-//     const parentClass = "div.mdl-grid";
-//     const cellClass = "mdl-cell mdl-cell--6-col";
-//     const nestedCell = "div.launch a6936 mdl-card mdl-shadow--6dp"
-//     const abcCell = "div.mdl-card__supporting-text"
-
-//     let client = ""
-    // const list = $(parentClass);
-    // let list = []
-    // let rockets = []
-    // list = $(parentClass).children().map( (i, element) => {
-    //     let rocket = { a: "", b: "", c: "", d: "", e: "", f: "", g: ""}
-    //     if (element.type ==='tag') {
-    //         let date = $(element).find(abcCell).text().trim().split("\n")[0].replace(",", "").split(" ")
-    //         switch (date.length) {
-    //             case(3): {
-    //                 rocket.a = date[2];
-    //                 rocket.b = date[1];
-    //                 break;
-    //             }
-    //             case(4 || 6): {
-    //                 rocket.a = date[3]
-    //                 rocket.b = date[1]
-    //                 rocket.c = date[2]
-    //                 break;
-    //             }
-    //             default: {
-    //                 break;
-    //             }
-    //         }
-    //         let title = $(element).find("h5:first").text().trim().split(" | ");
-    //         let origin = $(element).find("span:first").text().trim();
-    //         let details = $(element).find("div.mdl-card__supporting-text:first").text().trim().split("\n");
-    //         details.splice(0, 2);
-    //         let location = details[0].trim()
-    //         // console.log(details);
-    //         rocket.d = title[1];
-    //         rocket.e = origin;
-    //         rocket.f = title[0];
-    //         rocket.g = location;
-    //         // console.log(rocket);
-    //         rockets.push(rocket);
-    //         return rocket
-
-        // }
-    // })
-    // console.log(rockets)
-//     fs.writeFile("Clients.json", JSON.stringify(rockets, null, 2), (err) => {
-//       if (err) {
-//         console.error(err);
-//         return;
-//       }
-//       console.log("Successfully written data to file");
-//     });
-//     console.log("list:", list);
-//   } catch (err) {
-//     console.error(err);
-//   }
-// }
-// Invoke the above function
-// scrapeData();
